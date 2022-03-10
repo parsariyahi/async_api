9c2758ffddfe69194b69a044c1f05ed23b034006
parsariyahi <pany.parsariyahi@gmail.com>
2022-03-10 15:58:23 +0330

    created product routes | debug aouth2

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
-from typing import Optional
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, Form, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from jose import JWTError, jwt
+from typing import Optional, Any
 
 SECRET_KEY = 'de7cedf364a2bd8ace889a8179887ddbcbb474bcbb024dd782519f9b59e39d24'
 ALGORITHM = 'HS256'
+
+
+
+# --------------- database --------------- 
 
 user_fake_db = {
     'prrh' : {
@@ -38,7 +42,7 @@ product_fake_db = {
     2 : {
         'id' : 2,
         'name' : 'cream',
-        'price' : 1,
+        'price' : 7,
         'description' : 'vanilla cream',
         },
 
@@ -50,6 +54,10 @@ product_fake_db = {
         },
 }
 
+# --------------- database --------------- 
+
+
+# --------------- models --------------- 
 
 class Token(BaseModel):
     access_token : str
@@ -70,11 +78,31 @@ class User(BaseModel):
 class UserIn(User):
     password : str
 
+class Product(BaseModel) :
+    id : int
+    name : str
+    price : int
+    description : Optional[str]
+
+# --------------- models --------------- 
+
 
 scheme = OAuth2PasswordBearer(tokenUrl='token')
 
-
 app = FastAPI()
+
+
+
+# --------------- token functions  --------------- 
+
+# TODO create expire time for token
+def token_create(data : dict) :
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+# --------------- token functions  --------------- 
+
+
+# --------------- user functions  --------------- 
 
 # TODO hash passwords
 def user_get(db, username : str) :
@@ -94,10 +122,6 @@ def user_authenticate(db, username : str, password : str) :
     return None
     #return False
 
-# TODO create expire time for token
-def create_accsess_token(data : dict) :
-    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
-
 async def user_current_get(token : str = Depends(scheme)) :
     user = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     username = user.get('sub')
@@ -106,21 +130,115 @@ async def user_current_get(token : str = Depends(scheme)) :
         if user :
             return user
 
+    raise HTTPException(
+        status.HTTP_401_UNAUTHORIZED,
+        'invalid user'
+    )
+
+# --------------- user functions  --------------- 
+
+
+# --------------- product functions  --------------- 
+
+def product_get(db, id : int) -> Product : 
+    if id in db :
+        product = db[id]
+        return Product(**product)
+
+    return None
+    #return False
+
+def product_add(db, product: Product) :
+    if product.id in db :
+        return None, 5 
+
+    try :
+        db[product.id] = {
+                'id' : product.id,
+                'name' : product.name,
+                'price' : product.price,
+                'description' : product.description,
+        }
+
+        return product, 1
+
+    except :
+        return None, 0
+
+def product_update(db, id: int, key: Any, value: Any) :
+    if id in db :
+        db[id][key] = value
+        return True
+
+    return None
+
+def product_delete(db, id: int) :
+    if id in db :
+        del db[id]
+        return True
+
+    return None
+
+# --------------- product functions  --------------- 
+
+
+
 @app.post('/token', response_model=Token)
 async def login(form_data : OAuth2PasswordRequestForm = Depends()) :
     user = user_authenticate(user_fake_db, form_data.username, form_data.password)
 
     if user :
-        access_token = create_accsess_token({'sub' : user.username})
+        access_token = token_create({'sub' : user.username})
         return {'access_token' : access_token}
 
     # if user was not found return 401 error
-    return HTTPException(
+    raise HTTPException(
         status.HTTP_401_UNAUTHORIZED,
         'incorrect username or password',
     )
 
 
+# return aouthorized user info
 @app.get('/user/myinf/', response_model=User)
 async def user_info(current_user : User = Depends(user_current_get)) :
     return current_user
+
+# read a prudoct whith id
+@app.get('/prd/read/{id}', response_model=Product)
+async def get_product(id : int) :
+    return product_get(product_fake_db, id)
+
+
+# TODO create a better return with status codes
+# add new product
+@app.post('/prd/add/', status_code=status.HTTP_201_CREATED)
+async def add_product(product: Product) :
+    res, status = product_add(product_fake_db, product)
+
+    if res :
+        return {'result' : res, 'db' : product_fake_db}
+
+    if status == 5 :
+        return {'result' : 'the product is alredy exist'}
+
+    return {'result' : 'there is a problem! please try again'}
+
+# update a product with id
+@app.put('/prd/edit/{id}')
+async def update_product(id: int, key: Any, value: Any) :
+    res = product_update(product_fake_db, id, key, value)
+    
+    if res :
+        return {'result' : res, 'edited field' : product_fake_db[id]}
+    
+    return {'result' : False}
+
+# delete a product with id
+@app.delete('/prd/del/{id}')
+async def delete_product(id: int) :
+    res = product_delete(product_fake_db, id)
+
+    if res :
+        return {'result' : res, 'deleted field' : product_fake_db}
+
+    return {'result' : False}
